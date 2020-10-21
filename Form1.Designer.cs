437e79d8838b9ef2eb71e4be2690d22fe4a41852
Chandan Rath <chandan.rath@securemeters.com>
2020-10-21 18:16:14 +0530

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace SharpMapSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mapBox1 = new SharpMap.Forms.MapBox();
+            this.btnPan = new System.Windows.Forms.Button();
+            this.btnZoomIn = new System.Windows.Forms.Button();
+            this.btnZoomOut = new System.Windows.Forms.Button();
+            this.btnLayerExtent = new System.Windows.Forms.Button();
+            this.btnZoomWindow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mapBox1
+            // 
+            this.mapBox1.ActiveTool = SharpMap.Forms.MapBox.Tools.None;
+            this.mapBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mapBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.mapBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.mapBox1.CustomTool = null;
+            this.mapBox1.FineZoomFactor = 10D;
+            this.mapBox1.Location = new System.Drawing.Point(12, 41);
+            this.mapBox1.MapQueryMode = SharpMap.Forms.MapBox.MapQueryType.LayerByIndex;
+            this.mapBox1.Name = "mapBox1";
+            this.mapBox1.QueryGrowFactor = 5F;
+            this.mapBox1.QueryLayerIndex = 0;
+            this.mapBox1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
+            this.mapBox1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
+            this.mapBox1.ShowProgressUpdate = false;
+            this.mapBox1.Size = new System.Drawing.Size(486, 397);
+            this.mapBox1.TabIndex = 0;
+            this.mapBox1.Text = "mapBox1";
+            this.mapBox1.WheelZoomMagnitude = -2D;
+            // 
+            // btnPan
+            // 
+            this.btnPan.Location = new System.Drawing.Point(12, 12);
+            this.btnPan.Name = "btnPan";
+            this.btnPan.Size = new System.Drawing.Size(75, 23);
+            this.btnPan.TabIndex = 1;
+            this.btnPan.Text = "Pan";
+            this.btnPan.UseVisualStyleBackColor = true;
+            this.btnPan.Click += new System.EventHandler(this.btnPan_Click);
+            // 
+            // btnZoomIn
+            // 
+            this.btnZoomIn.Location = new System.Drawing.Point(93, 12);
+            this.btnZoomIn.Name = "btnZoomIn";
+            this.btnZoomIn.Size = new System.Drawing.Size(75, 23);
+            this.btnZoomIn.TabIndex = 2;
+            this.btnZoomIn.Text = "ZoomIn";
+            this.btnZoomIn.UseVisualStyleBackColor = true;
+            this.btnZoomIn.Click += new System.EventHandler(this.btnZoomIn_Click);
+            // 
+            // btnZoomOut
+            // 
+            this.btnZoomOut.Location = new System.Drawing.Point(174, 12);
+            this.btnZoomOut.Name = "btnZoomOut";
+            this.btnZoomOut.Size = new System.Drawing.Size(75, 23);
+            this.btnZoomOut.TabIndex = 3;
+            this.btnZoomOut.Text = "ZoomOut";
+            this.btnZoomOut.UseVisualStyleBackColor = true;
+            this.btnZoomOut.Click += new System.EventHandler(this.btnZoomOut_Click);
+            // 
+            // btnLayerExtent
+            // 
+            this.btnLayerExtent.Location = new System.Drawing.Point(336, 12);
+            this.btnLayerExtent.Name = "btnLayerExtent";
+            this.btnLayerExtent.Size = new System.Drawing.Size(75, 23);
+            this.btnLayerExtent.TabIndex = 4;
+            this.btnLayerExtent.Text = "LayerExtent";
+            this.btnLayerExtent.UseVisualStyleBackColor = true;
+            this.btnLayerExtent.Click += new System.EventHandler(this.btnLayerExtent_Click);
+            // 
+            // btnZoomWindow
+            // 
+            this.btnZoomWindow.Location = new System.Drawing.Point(255, 12);
+            this.btnZoomWindow.Name = "btnZoomWindow";
+            this.btnZoomWindow.Size = new System.Drawing.Size(75, 23);
+            this.btnZoomWindow.TabIndex = 5;
+            this.btnZoomWindow.Text = "ZoomWin";
+            this.btnZoomWindow.UseVisualStyleBackColor = true;
+            this.btnZoomWindow.Click += new System.EventHandler(this.btnZoomWindow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 450);
+            this.Controls.Add(this.btnZoomWindow);
+            this.Controls.Add(this.btnLayerExtent);
+            this.Controls.Add(this.btnZoomOut);
+            this.Controls.Add(this.btnZoomIn);
+            this.Controls.Add(this.btnPan);
+            this.Controls.Add(this.mapBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpMap.Forms.MapBox mapBox1;
+        private System.Windows.Forms.Button btnPan;
+        private System.Windows.Forms.Button btnZoomIn;
+        private System.Windows.Forms.Button btnZoomOut;
+        private System.Windows.Forms.Button btnLayerExtent;
+        private System.Windows.Forms.Button btnZoomWindow;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using BruTile;
+using BruTile.Predefined;
+using GeoAPI.CoordinateSystems;
+using GeoAPI.CoordinateSystems.Transformations;
+using NetTopologySuite;
+using ProjNet.CoordinateSystems;
+using ProjNet.CoordinateSystems.Transformations;
+using SharpMap;
+using SharpMap.Data.Providers;
+using SharpMap.Forms;
+using SharpMap.Layers;
+using SharpMap.Rendering.Symbolizer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SharpMapSample
+{
+    public partial class Form1 : Form
+    {
+        private static ICoordinateTransformation _wgs84ToGoogle;
+        private static ICoordinateTransformation _googletowgs84;
+
+        public Form1()
+        {
+            var gss = new NtsGeometryServices();
+            var css = new SharpMap.CoordinateSystems.CoordinateSystemServices(
+                new CoordinateSystemFactory(),
+                new CoordinateTransformationFactory(),
+                SharpMap.Converters.WellKnownText.SpatialReference.GetAllReferenceSystems());
+
+            GeoAPI.GeometryServiceProvider.Instance = gss;
+
+            SharpMap.Session.Instance
+                .SetGeometryServices(gss)
+                .SetCoordinateSystemServices(css)
+                .SetCoordinateSystemRepository(css);
+
+            InitializeComponent();
+
+            mapBox1.ActiveTool = MapBox.Tools.Pan;
+            mapBox1.Map.SRID = 3857;
+
+            LoadMap();
+        }
+
+        private void LoadMap()
+        {
+            Map map = new Map();
+
+            ITileSource tileSource = KnownTileSources.Create(KnownTileSource.OpenStreetMap);
+            TileAsyncLayer tileLayer = new TileAsyncLayer(tileSource, "Basemap");
+            map.BackgroundLayer.Add(tileLayer);
+
+
+            VectorLayer shpLayer = new VectorLayer("Points");
+            shpLayer.DataSource = new ShapeFile("Data/indian_points.shp", true);
+            shpLayer.Style.SymbolScale = 0.8f;
+            shpLayer.CoordinateTransformation = Wgs84toGoogleMercator; //Here it re-projects our degree decimal data to Google mercator
+            shpLayer.ReverseCoordinateTransformation = GoogleMercatorToWgs84;
+            shpLayer.SRID = 4326;
+
+            map.Layers.Add(shpLayer);
+
+            mapBox1.Map = map;
+
+            mapBox1.Map.ZoomToExtents();
+
+            mapBox1.Refresh();
+        }
+
+        public static ICoordinateTransformation Wgs84toGoogleMercator
+        {
+            get
+            {
+                if (_wgs84ToGoogle == null)
+                {
+                    CoordinateSystemFactory csFac = new ProjNet.CoordinateSystems.CoordinateSystemFactory();
+                    CoordinateTransformationFactory ctFac = new CoordinateTransformationFactory();
+
+                    IGeographicCoordinateSystem wgs84 = csFac.CreateGeographicCoordinateSystem(
+                      "WGS 84", AngularUnit.Degrees, HorizontalDatum.WGS84, PrimeMeridian.Greenwich,
+                      new AxisInfo("north", AxisOrientationEnum.North), new AxisInfo("east", AxisOrientationEnum.East));
+
+                    List<ProjectionParameter> parameters = new List<ProjectionParameter>();
+                    parameters.Add(new ProjectionParameter("semi_major", 6378137.0));
+                    parameters.Add(new ProjectionParameter("semi_minor", 6378137.0));
+                    parameters.Add(new ProjectionParameter("latitude_of_origin", 0.0));
+                    parameters.Add(new ProjectionParameter("central_meridian", 0.0));
+                    parameters.Add(new ProjectionParameter("scale_factor", 1.0));
+                    parameters.Add(new ProjectionParameter("false_easting", 0.0));
+                    parameters.Add(new ProjectionParameter("false_northing", 0.0));
+                    IProjection projection = csFac.CreateProjection("Google Mercator", "mercator_1sp", parameters);
+
+                    IProjectedCoordinateSystem epsg900913 = csFac.CreateProjectedCoordinateSystem(
+                      "Google Mercator", wgs84, projection, LinearUnit.Metre, new AxisInfo("East", AxisOrientationEnum.East),
+                      new AxisInfo("North", AxisOrientationEnum.North));
+
+                    ((CoordinateSystem)epsg900913).DefaultEnvelope = new[] { -20037508.342789, -20037508.342789, 20037508.342789, 20037508.342789 };
+
+                    _wgs84ToGoogle = ctFac.CreateFromCoordinateSystems(wgs84, epsg900913);
+                }
+
+                return _wgs84ToGoogle;
+            }
+        }
+
+        public static ICoordinateTransformation GoogleMercatorToWgs84
+        {
+            get
+            {
+                if (_googletowgs84 == null)
+                {
+                    CoordinateSystemFactory csFac = new ProjNet.CoordinateSystems.CoordinateSystemFactory();
+                    CoordinateTransformationFactory ctFac = new CoordinateTransformationFactory();
+
+                    IGeographicCoordinateSystem wgs84 = csFac.CreateGeographicCoordinateSystem(
+                      "WGS 84", AngularUnit.Degrees, HorizontalDatum.WGS84, PrimeMeridian.Greenwich,
+                      new AxisInfo("north", AxisOrientationEnum.North), new AxisInfo("east", AxisOrientationEnum.East));
+
+                    List<ProjectionParameter> parameters = new List<ProjectionParameter>();
+                    parameters.Add(new ProjectionParameter("semi_major", 6378137.0));
+                    parameters.Add(new ProjectionParameter("semi_minor", 6378137.0));
+                    parameters.Add(new ProjectionParameter("latitude_of_origin", 0.0));
+                    parameters.Add(new ProjectionParameter("central_meridian", 0.0));
+                    parameters.Add(new ProjectionParameter("scale_factor", 1.0));
+                    parameters.Add(new ProjectionParameter("false_easting", 0.0));
+                    parameters.Add(new ProjectionParameter("false_northing", 0.0));
+                    IProjection projection = csFac.CreateProjection("Google Mercator", "mercator_1sp", parameters);
+
+                    IProjectedCoordinateSystem epsg900913 = csFac.CreateProjectedCoordinateSystem(
+                      "Google Mercator", wgs84, projection, LinearUnit.Metre, new AxisInfo("East", AxisOrientationEnum.East),
+                      new AxisInfo("North", AxisOrientationEnum.North));
+
+                    _googletowgs84 = ctFac.CreateFromCoordinateSystems(epsg900913, wgs84);
+                }
+
+                return _googletowgs84;
+            }
+        }
+
+        private void btnPan_Click(object sender, EventArgs e)
+        {
+            mapBox1.ActiveTool = MapBox.Tools.Pan;
+        }
+
+        private void btnZoomIn_Click(object sender, EventArgs e)
+        {
+            mapBox1.ActiveTool = MapBox.Tools.ZoomIn;
+        }
+
+        private void btnZoomOut_Click(object sender, EventArgs e)
+        {
+            mapBox1.ActiveTool = MapBox.Tools.ZoomOut;
+        }
+
+        private void btnZoomWindow_Click(object sender, EventArgs e)
+        {
+            mapBox1.ActiveTool = MapBox.Tools.ZoomWindow;
+        }
+
+        private void btnLayerExtent_Click(object sender, EventArgs e)
+        {
+            mapBox1.Map.ZoomToBox(mapBox1.Map.Layers.First().Envelope);
+            mapBox1.Refresh();
+        }
+    }
+}
